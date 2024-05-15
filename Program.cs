05331c28319fd724d6b47a2b366c374fe0196367
ejanani <125354147+ejanani@users.noreply.github.com>
2024-05-15 13:27:52 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using LoanManagement.Service;
+namespace LoanManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ILoanService loanService = new LoanService();
+            loanService.GetAllLoan();
+            //loanService.GetLoanByID();
+            //loanService.ApplyLoan();
+            //loanService.calculateInterest();
+            //loanService.calculateEMI();
+            //loanService.loanRepayment();
+            //loanService.loanStatus();
+           
+        }
+    }
+}
