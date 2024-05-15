fffe5ad91ffebda412e06f5ad1ec4b6f2c8a25f2
ejanani <125354147+ejanani@users.noreply.github.com>
2024-05-15 13:26:41 +0530

    Add files via upload

---
+++ ILoanService.cs
@@ -0,0 +1,23 @@
+﻿using LoanManagement.Model;
+using LoanManagement.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanManagement.Service
+{
+    internal interface ILoanService
+    {
+
+        void GetAllLoan();
+        void GetLoanByID();
+        void ApplyLoan();
+        void calculateInterest();
+        void calculateEMI();
+        void loanRepayment();
+        void loanStatus();
+    }
+    }
+
